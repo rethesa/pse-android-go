25b4ac108806f241e16d67d60e604f53d1cf26e9
Victoria Karl <victoria.karl@web.de>
2017-01-07 13:02:02 +0100

    add Appointment

--- Entwurf/AndroidGoApp/AndroidGoApp/app/src/main/java/com/example/androidgoapp/androidgoapp/Model/keineAhnungWieIchDasNennenSoll/Appointment.java
+++ Entwurf/AndroidGoApp/AndroidGoApp/app/src/main/java/com/example/androidgoapp/androidgoapp/Model/keineAhnungWieIchDasNennenSoll/Appointment.java
@@ -1,43 +1,20 @@
 package com.example.androidgoapp.androidgoapp.Model.keineAhnungWieIchDasNennenSoll;
 
-import android.webkit.GeolocationPermissions;
-
-import java.sql.Time;
-import java.util.Date;
-
-//if destination and date is definabel individual, this class is waste
-
 /**
  * Created by Schokomonsterchen on 21.12.2016.
  */
 
 public class Appointment {
 
-    private GeolocationPermissions place;
-    private Date date;
-    private Time time;
+    private AppointmentDate appointmentDate;
+    private AppointmentDestination appointmentDestination;
 
-    public void setPlace(GeolocationPermissions place) {
-        this.place = place;
+    protected void setAppointmentDate(String stringDate) {
+        appointmentDate.setDate(stringDate);
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    protected void setAppointmentDestination(String stringappointmentDestination) {
+        appointmentDestination.setDestination(stringappointmentDestination);
     }
 
-    public void setTime(Time time) {
-        this.time = time;
-    }
-
-    public GeolocationPermissions getPlace() {
-        return place;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public Time getTime() {
-        return time;
-    }
 }

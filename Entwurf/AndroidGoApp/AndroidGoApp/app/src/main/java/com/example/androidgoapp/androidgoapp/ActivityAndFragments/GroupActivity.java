55efccf0234123375447dafbafca6b11ad626849
Theresa Heine <theresaheine@yahoo.de>
2017-01-08 16:39:26 +0100

    moved content for database from GroupActivity to MapActivity because this one ist the main activity

--- Entwurf/AndroidGoApp/AndroidGoApp/app/src/main/java/com/example/androidgoapp/androidgoapp/ActivityAndFragments/GroupActivity.java
+++ Entwurf/AndroidGoApp/AndroidGoApp/app/src/main/java/com/example/androidgoapp/androidgoapp/ActivityAndFragments/GroupActivity.java
@@ -12,17 +12,5 @@ import com.example.androidgoapp.androidgoapp.R;
 
 public class GroupActivity extends AppCompatActivity {
 
-    FeedReaderDbHelper mDbHelper;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group);
-        mDbHelper = new FeedReaderDbHelper(this);
-    }
-
-
-
-
 
 }
--- Entwurf/AndroidGoApp/AndroidGoApp/app/src/main/java/com/example/androidgoapp/androidgoapp/ActivityAndFragments/MapActivity.java
+++ Entwurf/AndroidGoApp/AndroidGoApp/app/src/main/java/com/example/androidgoapp/androidgoapp/ActivityAndFragments/MapActivity.java
@@ -1,11 +1,28 @@
 package com.example.androidgoapp.androidgoapp.ActivityAndFragments;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.androidgoapp.androidgoapp.Database.FeedReaderDbHelper;
+import com.example.androidgoapp.androidgoapp.R;
+
 /**
  * Created by Theresa on 21.12.2016.
  */
 
-public class MapActivity {
+public class MapActivity extends AppCompatActivity {
 
     //<GoService>
     //<Group.name>
+
+    FeedReaderDbHelper mDbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+        mDbHelper = new FeedReaderDbHelper(this);
+    }
+
+
 }

9b94c2822d76fc90cbf45e602f429590ae72cb90
Theresa Heine <theresaheine@yahoo.de>
2017-01-08 16:43:33 +0100

    created activity_map

--- Entwurf/AndroidGoApp/AndroidGoApp/app/src/main/java/com/example/androidgoapp/androidgoapp/ActivityAndFragments/GroupActivity.java
+++ Entwurf/AndroidGoApp/AndroidGoApp/app/src/main/java/com/example/androidgoapp/androidgoapp/ActivityAndFragments/GroupActivity.java
@@ -1,10 +1,6 @@
 package com.example.androidgoapp.androidgoapp.ActivityAndFragments;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.example.androidgoapp.androidgoapp.Database.FeedReaderDbHelper;
-import com.example.androidgoapp.androidgoapp.R;
 
 /**
  * Created by Theresa on 21.12.2016.

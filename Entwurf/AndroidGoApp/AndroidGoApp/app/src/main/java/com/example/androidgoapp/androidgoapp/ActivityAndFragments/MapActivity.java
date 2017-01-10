ce627786c71f2c8fa3fb022dddbdb145bf478005
Theresa Heine <theresaheine@yahoo.de>
2017-01-10 11:44:25 +0100

    adjusted MapActivity

--- Entwurf/AndroidGoApp/AndroidGoApp/app/src/main/java/com/example/androidgoapp/androidgoapp/ActivityAndFragments/MapActivity.java
+++ Entwurf/AndroidGoApp/AndroidGoApp/app/src/main/java/com/example/androidgoapp/androidgoapp/ActivityAndFragments/MapActivity.java
@@ -3,6 +3,7 @@ package com.example.androidgoapp.androidgoapp.ActivityAndFragments;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.androidgoapp.androidgoapp.Database.DBHelperGroup;
 import com.example.androidgoapp.androidgoapp.Database.kljdf.FeedReaderDbHelper;
 import com.example.androidgoapp.androidgoapp.R;
 
@@ -16,13 +17,13 @@ public class MapActivity extends AppCompatActivity {
     //<Group.name>
 
     //das ist im Moment doppelt
-    private FeedReaderDbHelper myDbHelper;
+    private DBHelperGroup myDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        myDbHelper = new FeedReaderDbHelper(this);
+        myDbHelper = new DBHelperGroup(this);
     }
 
        /*
